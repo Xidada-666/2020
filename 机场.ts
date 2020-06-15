31839ecb8b2784c1550a5a9c49424261ad06eaaf
Xidada-666 <66906553+Xidada-666@users.noreply.github.com>
2020-06-15 19:22:59 +0800

    1
    1

---
+++ 机场.ts
@@ -0,0 +1,279 @@
+
+如果打不开链接就是被墙了，可以先下载蓝灯或者使用免费SS/SSR节点
+
+推荐购买付费机场账号 ，花钱可以节省很多时间和精力,而且贵点的稳定点舒服很多，不可能又便宜又稳定流量还多的。用了好的就回不去了；下面有几个稳定的机场推荐。
+
+之前自己搭建虽然最安全，但是容易受到高墙干扰，维护很麻烦
+
+不推荐购买直接使用搜索vpn出来的软件，迟早药丸
+
+免费机场/白嫖机场一般不太稳定，而且比较危险；
+
+1.软件
+landeng ：https://github.com/getlantern/ （免费限制500M）
+
+下面有些链接被墙了,可以先用蓝灯打开
+
+赛风 ：https://psiphon3.com/zh/index.html （免费,不稳定）
+
+2.shadowsocks和V2ray：
+账号：
+
+付费机场：
+国内动不动封IP加干扰，用过不少便宜的机场，容易翻车,用着太糟心，所以最后还是加钱求稳定，舒服很多：
+
+vps 和线路注定，稳定&流量多&便宜==不存在
+
+
+规律：
+一分钱一分货，越贵越好，不推荐买直连ss/ssr机场，v2ray起速慢点。尽量选择有ILPC国际专线和BGP中转的，比较稳:
+
+便宜的机场只能买新机场，为了吸引人，但是久了人数多了机场主如果不加线路或者提高带宽必然拉跨速度降低并且不稳定。机场面板基本用的那几套，主要还是看线路质量。
+
+个人购买自用结果(买的都是不限制ip数目的)
+
+本人目前自用稳定高端机场:
+
+1.yoyu :
+
+ss/ssr 中转和专线机场,99条线路，有很多0.2和-0.5倍率节点（0.2倍率表示使用1G流量只算你200M，50G实际是100-250G），快又稳!（半年付起，提高了加入门槛，筛选优质用户）推荐 【pro套餐】:
+
+50g（0.3倍=167G /0.4倍=125G/0.5倍=100G）：342/年；
+
+100g（0.3倍=333G/0.4倍=250G/0.5倍=200G）：684/年；
+
+200g（0.3倍=667G/0.4倍=500G/0.5倍=400G）：1224/年；
+
+300g（0.3倍=1000G/0.4倍=750G/0.5倍=600G）：1530/年
+
+★★★★★，新用户需要通过购买套餐注册,官网： http://dlj.tf/EcazCR
+
+2.YTOO : v2ray全中转机场，也有ss节点，目前用着挺好,倍率0.2和0.6都有，速度一流（0.6倍率表示使用1G流量只算你600M,所以50G实际大概是80G）等，季付起，
+
+50G(0.2倍=250G/0.5倍=100G/0.6倍=83G) :249/年
+
+100G(0.2倍=500G/0.5倍=200G/0.6倍=166G):369/年
+
+200G(0.2倍=1000G/0.5倍=400G/0.6倍=332G):369/年
+
+300G(0.2倍=1500G/0.5倍=600G/0.6倍=480G):729/年
+
+★★★★☆，新用户需要通过购买套餐注册,官网： http://dlj.tf/2FvsU5
+
+其它用过的机场观察
+便宜佛系随缘机场：
+布丁 :v2ray机场，88块一年，每个月128G流量；128块一年的256G/月等套餐。有一块钱试用的套餐，可以先试试水感受感受再决定买不买。 官网：http://dlj.tf/MRGID5 ★★★☆
+
+可以和上面主用的对比下,价格决定了
+
+其它机场观察
+just my sock,搬瓦工官方的ss机场，CN2 GIA线路，速度是可以的；优点是比较安全，5条ip线路，5.88刀/月/500G起,5.2%折扣循环优惠码：JMS9272283 ；之前买了年费，目前改为aes-256-gcm加混淆后好多了;
+
+买了一年的搬瓦工的CN2 GIA服务器用这套配置也可以。在用 ★★☆
+
+极游 ：之前的万人3元便宜机场，买了第一个月的时候还行，买年费后就不行了，然后涨价了，被迫换套餐，但是现在也频繁崩。同时工单没有人处理，反正虽然便宜但不建议买。☆
+
+佩奇nex ：中转ss/ssr机场，80块一个月400G流量,略贵，月付的线路没有年付好，人数不少
+
+amytele :佩奇家分站，中转，25块60G，55块400G，不如直接买年费佩奇，人数不少，还行。
+
+N3RO : IPLC加中转线路,买过，还行，但是之前不省心的感觉
+
+海豚湾 ：买的团队套餐3000块/年。人太多，速度不行
+
+ssrcloud ： 平时还好，国庆不太稳定,但是便宜，公告老吹牛逼
+
+x-air ：(V2ray机场) 20一个月,只是进阶版；高级一般；年费才能高级版，110 半年1T 流量。线路比较偏
+
+CordCloud ：国庆伤亡惨重,但是便宜，现在人太多了明显，不推荐。☆
+
+其它未测试：
+bywave :中继和iplc， v2ray机场,25块50G起，有按流量计费套餐；据说还行；目前有89折优惠码WUHAN + 充值送活动；叠加优惠后年付50G的大概17块一个月 ,目前人数不多。 官网：https://dlj.tf/XFrHZF
+
+奶瓶 ：400G/25块/月,300G/45季度等。适合流量需求大的。主要是ssr，提供Trojan节点,据说价格，流量，稳定相对平衡。官网：http://dlj.tf/ivRcrE
+
+richcloud : 高贵，但是据说服务态度不好。
+
+魅影 ：不定时开放注册，谁会天天干等着。。。
+
+Dler ：之前机场主失踪好久，不管事，技术流=折腾，蛋疼
+
+202Net ：（中转机场） 50元/月 ,可以多客户端，但是不可以多ip
+
+梦迪 ：看上去还不错
+
+贼船 ：可以
+
+喵帕斯 :无限流量，看总IP时长。推荐3级，60块一个月，需要邀请码，算了算了。
+
+aaex : 100块/月；不限制流量；120G/588/年；以后拿来下数据。
+
+Blinkload ：(只能年费，开会期间bgp慢，youtube打了不少广告)
+
+V2rayPro ：据说稳定性不好
+
+次元链接 ：(伤亡惨重,不建议)
+
+墙裂 ：维护慢
+
+MunCloud：15块/月/100G起
+
+flycloud ：开会期间SSR全线瘫痪。剩余的V2之类的虽然可以用，但是极其不稳定。
+
+KyCloud ：国庆伤亡惨重
+
+其它：
+
+墙裂 ：维护非常慢
+
+心阶 ：5台客户端，半跑路，不稳定，别买
+
+蓝岸 ：只有年付,不稳定不推荐c
+
+厘米云iplcm ： 只有一个ip 限制客户端数目，不安全。
+
+咸鱼 ：：https://saltytuna.club/userinfo 不可以超过4个ip
+
+sockboom :10个客户端
+
+厘米云 ：只有一个ip 限制客户端数目
+
+cloudfree ：只有一个人使用，5块100g,15块300G
+
+免费机场/白嫖机场 ：
+免费节点不适合长时间使用，但是可以用来过渡打开暂时打不开的网站
+
+付费的机场也会有免费节SS/SSR点使用：
+
+速鹰666 ：http://dlj.tf/bkFUpq ；付费节点9块/100G/月起
+
+孤岛养鸡场 : http://dlj.tf/yCVgLQ ；付费的88GB/15.9块/月起
+
+金字塔 ：https://dlj.tf/gt1NNG，有免费中继节点
+
+糖果屋 ：http://www.52ss.club/
+
+DripCloud :https://dripcloud.net/
+
+闪电 ：http://dlj.tf/gDlOUx ;付费节点也挺便宜的
+
+good.mebi.me ：https://good.mebi.me
+
+sheepcloud ：https://www.sheepcloud.me/
+
+https://shadowsocks-share.herokuapp.com/
+
+https://www.ssrshare.com/
+
+中二病 ,https://zhoerbing.com/user，购买第一个输入优惠码：你才中二，
+
+kiki ：https://kiki666.com ，纯白嫖
+
+Eiz ：只有两条线， https://portal.eiz.me/
+
+司徒云 ： 中转线路，白嫖的某些节点无限流量的 https://situcloud.xyz
+
+星梦数据： https://hualuows.xyz/
+
+道长加速云 ： dzjs1.xyz/
+
+tan90 ： 比较冷门 https://tan90.best/auth/register?code=9OIj
+
+青青草原灰太狼 ： 白嫖俄罗斯节点 https://4312ru.xyz/auth/register?code=s5Zv
+
+需要先翻墙才打得开或者看得到：
+
+几鸡 ：https://ji-ag.pw/waf/ojZb2 ,每月初始10G流量，每天签到0.8~5GB
+
+neuk : https://neukssr.club/auth/register?code=0EDM
+
+free-ss ：https://free-ss.site
+
+https://lncn.org/
+
+https://www.ssrtool.com/tool/free_ssr
+
+http://ss.pythonic.life/full
+
+https://www.youneed.win/free-ssr
+
+白嫖免费机场telegram消息群：
+
+https://t.me/baipiaohao
+
+https://t.me/QBMWW
+
+可以直接打开：
+
+https://fanqiang.network/
+
+敌对势力提供的：https://github.com/Alvin9999/new-pac/wiki/ss免费账号
+
+另外一个大量收集的：https://github.com/hongwanzhang/Share-SSR-V2ray/blob/master/1-share-ssr-v2ray.md
+
+免费节点 | SS / SSR / V2Ray 节点分享
+
+客户端推荐使用clash ,可以定制规则，v2ray和ss 可以一起使用，自动选择最快的节点
+
+具体使用教程参考另外一篇博客：Clash各版本(win,mac,linux)使用教程(增加规则,搭配Proxy SwitchyOmega)： http://hongwan.xyz/2019/11/28/clash使用教程(搭配Proxy SwitchyOmega)/#more
+
+自己搭建,经常会被干扰，容易被封IP：
+账号可以自己买国外vps搭建shadowsocks（参考本博客其它博文）；成本越来越搞
+
+建议使用c版本的ss-libv,使用gcm加密+混淆
+
+shadowsocks客户端:
+
+windows、mac、安卓客户端:在github搜即可,机场一般也会有软件下载链接;
+
+ios客户端 ：Quantumult和shadowrocket,kitsunebi
+
+付费下载方法：国内appid 无法下载，最好通过外区账号购买下载；或者万能的闲鱼、转转、拼多多这些网站买个账号。
+
+免费下载方法：
+
+1.可以找网上的公共账号下载：比如微信公众号：SMG果粉
+
+2.通过下载好ipa文件安装包，然后通过PP助手/爱思助手安装:
+
+Shadowrocket/Surge/Kitsunebi的ipa安装包:
+
+https://github.com/ss-ssr/quan
+
+flyzy小站：https://www.flyzy2005.com/fan-qiang/ios-shadowrocket-surge-kitsunebi-ipa/
+
+V2Ray中文网:https://v2raycn.com/87.html
+
+Shadowrocket ipa文件：
+
+https://www.77nn.net/download?pid=1393
+
+V2Ray客户端：
+
+windows客户端 ：https://github.com/2dust/v2rayN
+
+mac客户端 ：https://github.com/yanue/V2rayU
+
+ios客户端 :kitsunebi
+
+安卓客户端 :V2RayNG ：https://github.com/2dust/v2rayNG/releases
+
+3.谷歌浏览器插件
+免费的小心被钓鱼和个人信息
+
+可以使用https://www.gugeapps.net/ 这个谷歌镜像镜像搜索安装；
+
+谷歌访问助手 ：http://www.ggfwzs.com/ ：免费使用12小时，永久使用会要求你绑定主页；口碑还行
+
+googlehelper :http://googlehelper.net/ (免费三天)
+
+setup vpn ：(免费)
+
+hoxx vpn proxy :（免费）
+
+skyZIP™ Proxy :（免费）
+
+4.改hosts
+可以使用工具一键更改，也可以手动改，主要是很容易失效，需要经常改，比较麻烦
+
+https://github.com/kelthuzadx/hosts
